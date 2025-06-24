bca67b21f6f3224065abf6d8df0dfe2c34da00fc
Pratyushaa94 <prathyusha9449@gmail.com>
2025-06-24 17:48:30 +0530

    changes in workflow

--- public_ip.tf
+++ public_ip.tf
@@ -1,6 +1,7 @@
 resource "azurerm_public_ip" "public_ip" {
   name                = "${var.vm_name}-public-ip"
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"  # <-- Changed from Basic to Standard
 }
